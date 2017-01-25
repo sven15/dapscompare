5f539896b30da6bc0ff1f5cb1ec2f5c408915999
sven15 <sseebergelverfeldt@suse.com>
2017-01-25 10:31:36 +0100

    set version to 0.3.0

--- packaging/dapscompare.spec
+++ packaging/dapscompare.spec
@@ -17,7 +17,7 @@
 
 
 Name:           dapscompare
-Version:        0.2.1
+Version:        0.3.0
 Release:        0
 Summary:        Detect Rendering Changes in Documentation Built with DAPS
 License:        MIT
--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup, find_packages
 
 setupdict = dict(
    name='dapscompare',
-   version='0.2.1',
+   version='0.3.0',
    description='dapscompare is a tool for comparing documentation output',
    url='https://github.com/openSUSE/dapscompare',
    # Author details
@@ -34,8 +34,7 @@ setupdict = dict(
    # simple. Or you can use find_packages().
    packages=find_packages('src'),
    package_dir={'': 'src'},
-   install_requires=[#'PyQt4',
-                     'scipy', 'numpy', 'Pillow'],
+   install_requires=['PyQt4','scipy', 'numpy', 'Pillow'],
    # have to be included in MANIFEST.in as well.
    package_data={
         '': ['src/dapscompare/README'],
