d97eb382b62270801c5de8b7810b4aada8a7f065
Sven Seeberg <sven@geeq.de>
2017-01-25 11:12:13 +0100

    Code refactoring (#23)
    * Refactoring (#20)
    * generate list of items to be rendered to images and then render, move code to renderers.py
    * combined filetypes parameters into one option
    * symlink to readme
    * flake8
    * streamlined runTests()
    * removed locks for output
    * set version to 0.3.0

--- src/dapscompare/helpers.py
+++ src/dapscompare/helpers.py
@@ -9,6 +9,16 @@
 # THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
 import os
+import multiprocessing
+import threading
+import queue
+import sys
+import json
+import shutil
+
+from scipy.misc import imsave, imread
+import numpy as np
+from PyQt4 import QtGui, QtCore
 
 def readFile(filePath):
 	if(os.path.isfile(filePath)):
@@ -19,16 +29,19 @@ def readFile(filePath):
 	else:
 		return False
 
+
 def writeFile(path,content):
 	f = open(path,'w')
 	f.write(content)
 	f.close()
+
 
 def modeToName(mode):
 	if mode == 1:
 		return "dapscompare-reference"
 	if mode == 2:
 		return "dapscompare-comparison"
+
 
 def hashPath(path):
 	import hashlib, os
@@ -44,9 +57,332 @@ def hashPath(path):
 				readFileBlock(filepath,SHAhash)
 	return SHAhash.hexdigest()
 
+
 def readFileBlock(filepath,SHAhash):
 	with open(filepath, 'rb') as f1:
 		while True:
 			buf = f1.read(4096)
 			if not buf : break
 			SHAhash.update(hashlib.sha1(buf).hexdigest())
+
+
+def registerHash(params,dataCollection):
+	import json, hashlib
+	# create md5sum of hash
+	hashstring = json.dumps(params, sort_keys=True)
+	md5 = hashlib.md5(hashstring.encode('utf-8'))
+	# add md5sum and string to config and save to file in the end
+	dataCollection.lock.acquire()
+	dataCollection.depHashes[md5.hexdigest()] = params
+	dataCollection.lock.release()
+	return md5.hexdigest()
+
+
+def listFiles(folder):
+	result = []
+	for item in os.listdir(folder):
+		if os.path.isfile(folder+item):
+			result.append(item)
+	return result
+
+
+class myWorkThread (QtCore.QThread):
+	# worker threads which compile the DC files and compare the results
+	def __init__(self, cfg, dataCollection, folders, foldersLock, threadID, name, counter):
+		QtCore.QThread.__init__(self)
+		self.threadID = threadID
+		self.name = name
+		self.counter = counter
+		self.cfg = cfg
+		self.dataCollection = dataCollection
+		self.folders = folders
+		self.foldersLock = foldersLock
+
+	def __del__(self):
+		self.wait()
+
+	def run(self):
+		from .daps import daps
+		# we want the threads to keep running until the queue of test cases is empty
+		while True:
+			testcase = ""
+			self.foldersLock.acquire()
+			if(self.folders.empty() == False):
+				testcase = self.folders.get()
+			self.foldersLock.release()
+			# finish the thread if queue is empty
+			if(testcase == ""):
+				break
+			print(self.name+" now working on "+testcase)
+
+			cleanDirectories(self.cfg, testcaseSubfolders=['build'], rmConfigs=False, testcase=testcase)
+			
+			# compile DC files
+			daps(testcase, self.cfg.dapsParam, self.cfg.filetypes)
+
+			# render results to images
+			runRenderers(self.cfg, self.dataCollection, testcase)
+
+			if(self.cfg.mode == 2):
+				runTests(self.cfg, self.dataCollection, testcase)
+		print(self.name+" finished")
+
+
+# prepare for file types and then call the appropriate rendering modules
+def runRenderers(cfg, dataCollection, testcase):
+	from .renderers import pdfItems, renderPdf, htmlItems, singleHtmlItems, epubItems, renderHtml
+	for filetype in cfg.filetypes:
+		if filetype == 'pdf':
+			for renderItem in pdfItems(testcase, cfg, dataCollection):
+				renderPdf(renderItem[0], renderItem[1], renderItem[2])
+		elif filetype == 'html' and cfg.noGui is False:
+			for renderItem in htmlItems(testcase, cfg, dataCollection):
+				renderHtml(renderItem[0], renderItem[1], renderItem[2])
+		elif filetype == 'single-html' and cfg.noGui is False:
+			for renderItem in singleHtmlItems(testcase, cfg, dataCollection):
+				renderHtml(renderItem[0], renderItem[1], renderItem[2])
+		elif filetype == 'epub' and cfg.noGui is False:
+			for renderItem in epubItems(testcase, cfg, dataCollection):
+				renderHtml(renderItem[0], renderItem[1], renderItem[2])
+
+
+# diff images of reference and compare run and save result
+def runTests(cfg, dataCollection, testcase):
+	for md5, description in dataCollection.depHashes.items():
+		referencePath = testcase+"dapscompare-reference/"+md5+"/"
+		comparisonPath = testcase+"dapscompare-comparison/"+md5+"/"
+		numRefImgs = len(listFiles(referencePath))
+		numComImgs = len(listFiles(comparisonPath))
+		if (numRefImgs - numComImgs) != 0 and numRefImgs != 0:
+			dataCollection.addDiffNumPages([referencePath, numRefImgs, numComImgs])
+			continue
+		cleanDirectories(cfg, testcaseSubfolders=['dapscompare-comparison', 'dapscompare-result'], rmConfigs=False, keepDirs=True, testcase=testcase)
+		if not os.path.exists(referencePath):
+			print("No reference images for "+dataCollection.depHashes[md5])
+			continue
+		diffFolder = testcase+"dapscompare-result/"+md5+"/"
+		if not os.path.exists(diffFolder):
+			os.makedirs(diffFolder)
+
+		for filename in os.listdir(referencePath):
+			imgRef = imread(referencePath+filename)
+			imgComp = imread(comparisonPath+filename)
+			try:
+				imgDiff = imgRef - imgComp
+				if np.count_nonzero(imgDiff) > 0:
+					imsave(diffFolder+filename, imgDiff)
+					dataCollection.addImgDiffs([referencePath+filename, comparisonPath+filename, diffFolder+filename])
+			except:
+				dataCollection.addDiffNumPages([referencePath, numRefImgs, numComImgs])
+
+
+class MyConfig:
+	def __init__(self):
+
+		self.stdValues()
+
+		self.cmdParams()
+
+		if self.loadConfigBool:
+			self.loadConfig()
+
+	def cmdParams(self):
+		# first read CLI parameters
+		for parameter in sys.argv:
+			if parameter == "compare":
+				self.mode = 2
+			elif parameter == "reference":
+				self.mode = 1
+			elif parameter == "view":
+				self.mode = 3
+			elif parameter == "clean":
+				self.mode = 4
+			elif parameter == "--help":
+				from dapscompare import __file__ as dapscomparedir
+				f = open(os.path.join(os.path.dirname(os.path.realpath(dapscomparedir)), 'README'), 'r')
+				print(f.read())
+				f.close()
+				sys.exit()
+			elif parameter == "--no-gui":
+				self.noGui = True
+			elif parameter.startswith("--daps="):
+				self.dapsParam = parameter[7:]
+			elif parameter.startswith("--testcases="):
+				self.directory = parameter[12:]
+			elif parameter.startswith("--filetypes="):
+				self.filetypes = parameter[13:].split(",")
+			elif parameter.startswith("--html-width="):
+				self.htmlWidth = parameter[13:].split(",")
+			elif parameter == "--ignore-conf":
+				self.loadConfigBool = False
+
+	def stdValues(self):
+		self.resDiffFile = "dapscompare-diff.json"
+		self.resHashFile = "dapscompare-hash.json"
+
+		# set standard values for all other needed parameters
+		self.directory = os.getcwd()+"/"
+
+		# 1 = build reference
+		# 2 = build comparison and run tests (standard)
+		# 3 = view results of last run
+		# 4 = clean
+		self.mode = 0
+
+		# usually show GUI after comparison
+		if "DISPLAY" in os.environ:
+			self.noGui = False
+		else:
+			self.noGui = True
+
+		if self.noGui:
+			self.filetypes = ['pdf']
+		else:
+			self.filetypes = ['pdf', 'html', 'single-html', 'epub']
+
+		self.htmlWidth = [1280]
+
+		self.dapsParam = "--force"
+
+		self.loadConfigBool = True
+
+	def loadConfig(self):
+		content = readFile(self.directory+"/"+self.resHashFile)
+		if content:
+			self.filetypes = []
+			content = json.loads(content)
+			for hashsum in content:
+				if content[hashsum]['Type'] not in self.filetypes:
+					self.filetypes.append(content[hashsum]['Type'])
+				if content[hashsum]['Type'] == "html":
+					if int(content[hashsum]['Width']) not in self.htmlWidth:
+						self.htmlWidth.append(int(content[hashsum]['Width']))
+
+
+class DataCollector:
+	def __init__(self,cfg):
+
+		self.lock = threading.Lock()
+
+		# if reference and comparison differ in number of pictures, store this here
+		self.diffNumPages = []
+
+		# compare or reference mode, new empty diff list
+		self.imgDiffs = []
+		# view mode, load existing diff list
+		if cfg.mode == 3:
+			imagesList = readFile(cfg.directory+cfg.resDiffFile)
+			if imagesList is False:
+				print("Nothing to do.")
+				sys.exit()
+			self.imgDiffs, self.diffNumPages = json.loads(imagesList)
+
+		# hashes of dependencies like image width and filetype
+		self.depHashes = {}
+		fileContent = readFile(cfg.directory+cfg.resHashFile)
+		if (fileContent is not False and len(fileContent) > 2):
+			self.depHashes = json.loads(fileContent)
+
+
+	def addDiffNumPages(self, item):
+		self.lock.acquire()
+		self.diffNumPages.append(item)
+		self.lock.release()
+
+
+	def addImgDiffs(self, item):
+		self.lock.acquire()
+		self.imgDiffs.append(item)
+		self.lock.release()
+
+def spawnWorkerThreads(cfg, dataCollection):
+	# get number of available cpus.
+	# we want to compile as many test cases with daps at the same time
+	# as we can
+
+	print("\n=== Parameters ===\n")
+
+	cpus = multiprocessing.cpu_count()
+	print("Number of CPUs: "+str(cpus))
+	print("Working Directory: "+cfg.directory)
+	print("Building: "+str(cfg.filetypes))
+
+	threads = []
+
+	folders, foldersLock = queueTestcases(cfg)
+
+	if folders.qsize() < cpus:
+		cpus = folders.qsize()
+
+	print("\n=== Creating "+str(cpus)+" Threads ===\n")
+
+	for threadX in range(0, cpus):
+		thread = myWorkThread(cfg, dataCollection, folders, foldersLock, threadX, "Thread-"+str(threadX), threadX)
+		thread.start()
+		threads.append(thread)
+
+	# Wait for all threads to complete
+	for t in threads:
+		t.wait()
+	print("All threads finished.")
+	if cfg.mode == 2:
+		writeFile(cfg.directory+cfg.resDiffFile, json.dumps([dataCollection.imgDiffs, dataCollection.diffNumPages]))
+	writeFile(cfg.directory+cfg.resHashFile, json.dumps(dataCollection.depHashes))
+
+
+def queueTestcases(cfg, silent=False):
+	folders = queue.Queue()
+	foldersLock = threading.Lock()
+	if not silent:
+		print("\n=== Test Cases ===\n")
+	n = 1
+	foldersLock.acquire()
+	for testcase in findTestcases(cfg):
+		if not silent:
+			print(str(n)+". "+testcase)
+			n = n + 1
+		folders.put(cfg.directory+testcase+"/")
+	foldersLock.release()
+	return folders, foldersLock
+
+
+def findTestcases(cfg):
+	for testcase in os.listdir(cfg.directory):
+		if(os.path.isdir(cfg.directory+"/"+testcase)):
+			yield testcase
+
+
+def cleanDirectories(cfg, testcaseSubfolders=['dapscompare-reference', 'dapscompare-comparison', 'dapscompare-result', 'build'], rmConfigs=True, testcase=False, keepDirs=False):
+	if testcase is False:
+		testcases = findTestcases(cfg)
+	else:
+		testcases = [testcase]
+
+	for testcase in testcases:
+		for subfolder in testcaseSubfolders:
+			try:
+				if keepDirs:
+					shutil.rmtree(testcase+"/"+subfolder+"/*")
+				else:
+					shutil.rmtree(testcase+"/"+subfolder)
+			except:
+				pass
+	if rmConfigs:
+		try:
+			os.remove(cfg.directory+cfg.resHashFile)
+		except:
+			pass
+		try:
+			os.remove(cfg.directory+cfg.resDiffFile)
+		except:
+			pass
+
+
+def printResults(dataCollection):
+	print("\n=== Changed Images ===\n")
+	for item in dataCollection.imgDiffs:
+		print(item[0])
+	print("\n=== Differing Page Numbers ===\n")
+	for item in dataCollection.diffNumPages:
+		print(item[0])
+	print()
--- src/dapscompare/renderers.py
+++ src/dapscompare/renderers.py
@@ -10,6 +10,7 @@
 
 import os
 from subprocess import check_output, Popen, PIPE
+from .helpers import modeToName, registerHash
 
 def renderHtml(pathHtml,pageWidth,pathPng):
 	# convert all PDF pages into numbered images and place them in reference or comparison folder
@@ -37,3 +38,54 @@ def renderPdfWand(pathPdf, pathPngDir):
 			#i.alpha_channel = False
 			#i.save(filename=pathPngDir+'/page-%s.png' % x)
 			i.save(filename=pathPngDir+'/page-%03d.png')
+
+#find the PDF files in build folder and convert to png
+def pdfItems(testcase,cfg,dataCollection):
+	folderName = testcase+modeToName(cfg.mode)+"/"+registerHash({'Type': 'pdf'},dataCollection)
+	if not os.path.exists(folderName):
+		os.makedirs(folderName)
+	yield (testcase+"build/*/*.pdf",100,folderName)
+
+#find HTML files in build folder and convert to png
+def htmlItems(testcase,cfg,dataCollection):
+	for build in os.listdir(testcase+"build"):
+		if not build.startswith("."):
+			for htmlBuild in os.listdir(testcase+"build/"+build+"/html/"):
+				for htmlFile in os.listdir(testcase+"build/"+build+"/html/"+htmlBuild):
+					for width in cfg.htmlWidth:
+						folderName = testcase+modeToName(cfg.mode)+"/"+registerHash({'Type': 'html', 'Width': str(width), 'File Name': htmlBuild+htmlFile},dataCollection)+"/"
+						if not os.path.exists(folderName):
+							os.makedirs(folderName)
+						if not os.path.islink(testcase+"build/"+build+"/html/"+htmlBuild+"/"+htmlFile):
+							yield (testcase+"build/"+build+"/html/"+htmlBuild+"/"+htmlFile,width,folderName)
+
+#find Single HTML files in build folder and convert to png
+def singleHtmlItems(testcase,cfg,dataCollection):
+	for build in os.listdir(testcase+"build"):
+		if not build.startswith("."):
+			for htmlBuild in os.listdir(testcase+"build/"+build+"/single-html/"):
+				for htmlFile in os.listdir(testcase+"build/"+build+"/single-html/"+htmlBuild):
+					for width in cfg.htmlWidth:
+						folderName = testcase+modeToName(cfg.mode)+"/"+registerHash({'Type': 'single-html', 'Width': str(width), 'File Name': htmlBuild+htmlFile},dataCollection)+"/"
+						if not os.path.exists(folderName):
+							os.makedirs(folderName)
+						if not os.path.islink(testcase+"build/"+build+"/single-html/"+htmlBuild+"/"+htmlFile):
+							yield (testcase+"build/"+build+"/single-html/"+htmlBuild+"/"+htmlFile,width,folderName)
+
+#find EPUB files in build folder and convert to png
+def epubItems(testcase,cfg,dataCollection):
+	import zipfile
+	for build in os.listdir(testcase+"build"):
+		if not build.startswith("."):
+			for epub in os.listdir(testcase+"build/"+build+"/"):
+				if epub.endswith(".epub"):
+					os.makedirs(testcase+"build/"+build+"/"+epub[0:-5]+"/")
+					with zipfile.ZipFile(testcase+"build/"+build+"/"+epub, 'r') as zip_ref:
+						zip_ref.extractall(testcase+"build/"+build+"/"+epub[0:-5]+"/")
+					for htmlFile in os.listdir(testcase+"build/"+build+"/"+epub[0:-5]+"/OEBPS/"):
+						for width in cfg.htmlWidth:
+							folderName = testcase+modeToName(cfg.mode)+"/"+registerHash({'Type': 'epub', 'Width': str(width), 'File Name': epub},dataCollection)+"/"
+							if not os.path.exists(folderName):
+								os.makedirs(folderName)
+							if not os.path.islink(testcase+"build/"+build+"/"+epub[0:-5]+"/OEBPS/"+htmlFile):
+								yield (testcase+"build/"+build+"/"+epub[0:-5]+"/OEBPS/"+htmlFile,width,folderName)
