28b0a5b190ac229daf9ca05f6234655376a11fb8
sven15 <sseebergelverfeldt@suse.com>
2017-10-17 14:36:07 +0200

    Fixed compare run, Added JSON output

--- src/dapscompare/daps.py
+++ src/dapscompare/daps.py
@@ -12,75 +12,75 @@ from subprocess import check_output, Popen, PIPE
 import os
 
 class daps:
-	def __init__(self, testcase, dapsParam, filetypes = []):
-		self.testcase = testcase
-		self.filetypes = filetypes
-		self.dapsParam = dapsParam
-		
-		self.createFolders()
-		
-		self.dcFiles = []
-		
-		self.findDcFiles()
-		
-		self.success = self.compileAllWait()
+    def __init__(self, testcase, dapsParam, filetypes = []):
+        self.testcase = testcase
+        self.filetypes = filetypes
+        self.dapsParam = dapsParam
 
-	def findDcFiles(self):
-		for filename in os.listdir(self.testcase):
-			if(filename[0:2] == "DC"):
-				self.dcFiles.append(self.testcase+filename)
-				
-	def createFolders(self):
-		# create dapscompare target folders
-		for filetype in self.filetypes:
-			for targetfolder in ['/dapscompare-comparison/','/dapscompare-reference/','/dapscompare-result/']:
-				if not os.path.exists(self.testcase+targetfolder):
-					os.makedirs(self.testcase+targetfolder)
+        self.createFolders()
 
-	def compilePdf(self):
-		my_env = os.environ.copy()
-		for dcFile in self.dcFiles:
-			self.procDaps = Popen(["cd "+self.testcase+" && /usr/bin/daps "+self.dapsParam+" -d "+dcFile+" pdf"], env=my_env, shell=True, stdout=PIPE, stderr=PIPE)
-			self.procDaps.wait()
-			result = str(self.procDaps.returncode)
-			return result
+        self.dcFiles = []
 
-	def compileHtml(self):
-		my_env = os.environ.copy()
-		for dcFile in self.dcFiles:
-			self.procHtml = Popen(["cd "+self.testcase+" && /usr/bin/daps "+self.dapsParam+" -d "+dcFile+" html"], env=my_env, shell=True, stdout=PIPE, stderr=PIPE)
-			self.procHtml.wait()
-			result = str(self.procHtml.returncode)
-			return result
+        self.findDcFiles()
 
-	def compileSingleHtml(self):
-		my_env = os.environ.copy()
-		for dcFile in self.dcFiles:
-			self.procHtml = Popen(["cd "+self.testcase+" && /usr/bin/daps "+self.dapsParam+" -d "+dcFile+" html --single"], env=my_env, shell=True, stdout=PIPE, stderr=PIPE)
-			self.procHtml.wait()
-			result = str(self.procHtml.returncode)
-			return result
+        self.success = self.compileAllWait()
 
-	def compileEpub(self):
-		my_env = os.environ.copy()
-		for dcFile in self.dcFiles:
-			self.procEpub = Popen(["cd "+self.testcase+" && /usr/bin/daps "+self.dapsParam+" -d "+dcFile+" epub"], env=my_env, shell=True, stdout=PIPE, stderr=PIPE)
-			self.procEpub.wait()
-			result = str(self.procEpub.returncode)
-			return result
+    def findDcFiles(self):
+        for filename in os.listdir(self.testcase):
+            if(filename[0:2] == "DC"):
+                self.dcFiles.append(self.testcase+filename)
 
-	def compileAllWait(self):
-		success = []
-		if 'pdf' in self.filetypes:
-			if self.compilePdf() == "0":
-				success.append('pdf')
-		if 'html' in self.filetypes:
-			if self.compileHtml() == "0":
-				success.append('html')
-		if 'single-html' in self.filetypes:
-			if self.compileSingleHtml() == "0":
-				success.append('single-html')
-		if 'epub' in self.filetypes:
-			if self.compileEpub() == "0":
-				success.append('epub')
-		return success
+    def createFolders(self):
+        # create dapscompare target folders
+        for filetype in self.filetypes:
+            for targetfolder in ['/dapscompare-comparison/','/dapscompare-reference/','/dapscompare-result/']:
+                if not os.path.exists(self.testcase+targetfolder):
+                    os.makedirs(self.testcase+targetfolder)
+
+    def compilePdf(self):
+        my_env = os.environ.copy()
+        for dcFile in self.dcFiles:
+            self.procDaps = Popen(["cd "+self.testcase+" && /usr/bin/daps "+self.dapsParam+" -d "+dcFile+" pdf"], env=my_env, shell=True, stdout=PIPE, stderr=PIPE)
+            self.procDaps.wait()
+            result = str(self.procDaps.returncode)
+            return result
+
+    def compileHtml(self):
+        my_env = os.environ.copy()
+        for dcFile in self.dcFiles:
+            self.procHtml = Popen(["cd "+self.testcase+" && /usr/bin/daps "+self.dapsParam+" -d "+dcFile+" html"], env=my_env, shell=True, stdout=PIPE, stderr=PIPE)
+            self.procHtml.wait()
+            result = str(self.procHtml.returncode)
+            return result
+
+    def compileSingleHtml(self):
+        my_env = os.environ.copy()
+        for dcFile in self.dcFiles:
+            self.procHtml = Popen(["cd "+self.testcase+" && /usr/bin/daps "+self.dapsParam+" -d "+dcFile+" html --single"], env=my_env, shell=True, stdout=PIPE, stderr=PIPE)
+            self.procHtml.wait()
+            result = str(self.procHtml.returncode)
+            return result
+
+    def compileEpub(self):
+        my_env = os.environ.copy()
+        for dcFile in self.dcFiles:
+            self.procEpub = Popen(["cd "+self.testcase+" && /usr/bin/daps "+self.dapsParam+" -d "+dcFile+" epub"], env=my_env, shell=True, stdout=PIPE, stderr=PIPE)
+            self.procEpub.wait()
+            result = str(self.procEpub.returncode)
+            return result
+
+    def compileAllWait(self):
+        success = []
+        if 'pdf' in self.filetypes:
+            if self.compilePdf() == "0":
+                success.append('pdf')
+        if 'html' in self.filetypes:
+            if self.compileHtml() == "0":
+                success.append('html')
+        if 'single-html' in self.filetypes:
+            if self.compileSingleHtml() == "0":
+                success.append('single-html')
+        if 'epub' in self.filetypes:
+            if self.compileEpub() == "0":
+                success.append('epub')
+        return success
